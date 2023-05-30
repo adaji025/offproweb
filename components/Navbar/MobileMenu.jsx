bbc30eb92bcecf6edda8015ccd8ccc33d298b91d
Adaji Mukhtar <alfadaji@gmail.com>
2023-05-30 19:21:18 +0100

    made changes to mobile navbar

--- components/Navbar/MobileMenu.jsx
+++ components/Navbar/MobileMenu.jsx
@@ -7,10 +7,10 @@ const MobileMenu = ({ isMobileMenuOpen }) => {
     <div
       className={`${
         isMobileMenuOpen ? "left-0" : "left-[-100%]"
-      } absolute top-[100px] left-0 w-full bg-green shadow z-20 transition-all duration-300`}
+      } absolute top-[100px] left-0 w-full bg-green shadow z-20 transition-all duration-300 md:hidden`}
       id="mobile-menu"
     >
-      <div className="px-4 py-5 h-[40vh] flex flex-col justify-center items-center text-white">
+      <div className="px-4 py-5 h-min flex flex-col justify-center items-center text-white">
         <div
          onClick={() => router.push("/about")}
           className="block text-gray-800 font-semibold py-2 hover:text-black/90 transition-all duration-300 cursor-pointer"
