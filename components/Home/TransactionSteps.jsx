6d866804765f4ffa053dc8246928b72e6b0cd742
Adaji Mukhtar <alfadaji@gmail.com>
2023-06-05 16:35:16 +0100

    made amend to step section

--- components/Home/TransactionSteps.jsx
+++ components/Home/TransactionSteps.jsx
@@ -14,38 +14,48 @@ const TransactionSteps = () => {
         </p>
         <div className="lg:flex gap-8 mt-[60px] lg:mt-[112px]">
           <div
-            className="rounded-lg bg-white shadow-sm mb-16 lg:mb-0 pl-3 pt-3 sm:w-[383px] mx-auto lg:w-[unset] min-h-[386px] relative"
+            className="rounded-lg bg-white shadow-sm mb-16 lg:mb-0 pl-3 pt-3 sm:w-[383px] mx-auto min-h-[300px] relative"
             data-aos="fade-right"
           >
-            <p className="text-[22px] lg:text-2xl lg:w-[90%] mb-10  font-bold mt-4 -mr-4 ">
-              Press the button on the left to{" "}
-              <span className="text-green">turn on the OPD</span>
-            </p>
-            <img
-              src="/images/opd-1.png"
-              alt=""
-              className="absolute right-0 bottom-0"
-            />
+            <div className="mt-4">
+              <h3 className="text-[22px] lg:text-2xl lg:w-[90%]   font-bold ">
+                Initiate and
+                <span className="text-green"> Confirm:</span>
+              </h3>
+              <p>
+                {" "}
+                Input the payment details and authorize the transaction with a
+                secure <span className="text-green"> PIN.</span>{" "}
+              </p>
+            </div>
+            <img src="/images/step1.png" alt="" className="pb-4" />
           </div>
           <div
-            className="rounded-lg shadow-sm bg-white mb-16 lg:mb-0 pl-3 pt-3 sm:w-[383px] mx-auto lg:w-[unset] min-h-[386px]"
+            className="rounded-lg shadow-sm bg-white mb-16 lg:mb-0 pl-3 pt-3 sm:w-[383px] mx-auto  min-h-[350px]"
             data-aos="fade-down"
           >
-            <p className="text-[22px] lg:text-2xl lg:w-[90%] mb-10 font-bold h-[100px] translate-y-6">
-              Press the up arrow button on the right{" "}
-              <span className="text-green">to send e-naira</span>
-            </p>
-            <img src="/images/opd-2.png" alt="" />
+            <div className="mt-4">
+              <h3 className="text-[22px] lg:text-2xl lg:w-[90%]   font-bold ">
+                Tap:
+              </h3>
+              <p> Simply tap your Tappr device or smartphone to transact.</p>
+            </div>
+            <img src="/images/step2.png" alt="" />
           </div>
           <div
-            className="rounded-lg shadow-sm pl-3 pt-3 bg-white sm:w-[383px] mx-auto lg:w-[unset] min-h-[386px]"
+            className="rounded-lg shadow-sm pl-3 pt-3 bg-white sm:w-[383px] mx-auto min-h-[350px]"
             data-aos="fade-left"
           >
-            <p className="text-x[22px] sm:text-2xl lg:w-[90%] mb-10 font-bold h-[100px] translate-y-6">
-              Press the button on the right to{" "}
-              <span className="text-green">turn on the OPD</span>
-            </p>
-            <img src="/images/opd-3.png" alt="" />
+            <div className="mt-4">
+              <h3 className="text-[22px] lg:text-2xl font-bold ">
+                Go:
+              </h3>
+              <p>
+                Enjoy the speed and convenience of a cashless transaction as you
+                swiftly complete your purchase or transfer funds.
+              </p>
+            </div>
+            <img src="/images/step3.png" alt="" />
           </div>
         </div>
       </div>
