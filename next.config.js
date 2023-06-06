94af1530116d63334c2c1fa27745d736e88c73c2
Adaji Mukhtar <alfadaji@gmail.com>
2023-06-06 15:23:29 +0100

    replaced the default favicon

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  head: {
+    // Add the link element for the favicon
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png', // Set the appropriate MIME type for your favicon
+        href: '/offpro.png' // Update the path and filename if necessary
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
